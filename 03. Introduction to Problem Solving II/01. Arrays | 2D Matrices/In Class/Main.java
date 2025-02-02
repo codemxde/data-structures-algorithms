eb9534394ee3bfe9df3165770b55307a069c8ec1
Keshav Dutt Kaushik <codemxde23@gmail.com>
2025-02-02 15:09:39 +0530

    started in-class questions for 2d matrices

--- 03. Introduction to Problem Solving II/01. Arrays | 2D Matrices/In Class/Main.java
+++ 03. Introduction to Problem Solving II/01. Arrays | 2D Matrices/In Class/Main.java
@@ -22,8 +22,56 @@ public class Main {
         System.out.println("-----------------------------------------------------");
     }
 
-    public static void rowSum(int[][] arr, Scanner sc) {
+    public static void printMatrix(int[][] arr) {
+        int r = arr.length, c = arr[0].length;
+        for (int i = 0; i < r; i++) {
+            System.out.print("sum for row " + (i+1) + ": [");
+            for (int j = 0; j < c; j++) {
+                int ele = arr[i][j];
+                System.out.print(j != c - 1 ? ele + ", " : ele + "]\n");
+            }
+        }
+    }
+
+    public static void rowSum(int[][] arr) {
         System.out.println("Q1. Given arr[N][M], print row-wise sum.\n");
+        int r = arr.length, c = arr[0].length;
+        for (int i = 0; i < r; i++) {
+            int sum = 0;
+            for (int j = 0; j < c; j++) {
+                sum += arr[i][j];
+            }
+            System.out.print("sum for row " + (i+1) + ": " + sum + "\n");
+        }
+    }
+
+    public static void columnWiseSum(int[][] arr) {
+        System.out.println("Q2. Given arr[N][M], print column-wise sum.\n");
+        int r = arr.length, c = arr[0].length;
+        for (int i = 0; i < c; i++) {
+            int sum = 0;
+            for (int j = 0; j < r; j++) {
+                sum += arr[j][i];
+            }
+            System.out.print("sum for column " + (c+1) + ": " + sum + "\n");
+        }
+    }
+
+    public static void printDiagonals(int[][] arr) {
+        System.out.println("Q3. Given arr[N][N]. Print Digaonals\n");
+        int r = arr.length;
+
+        System.out.print("right diagonal elements of the matrix: [");
+        for (int i = 0; i < r; i++) {
+            int diagonal = arr[i][i];
+            System.out.print(i != r-1 ? diagonal + ", " : diagonal + "]\n");
+        }
+
+        System.out.print("left diagonal elements of the matrix: [");
+        for (int i = 0; i < r; i++) {
+            int diagonal = arr[i][r-i-1];
+            System.out.print(i != r-1 ? diagonal + ", " : diagonal + "]\n");
+        }
     }
 
     public static void main(String[] args) {
@@ -33,6 +81,12 @@ public class Main {
         int[][] arr = readMatrix(sc); endOfSegment();
 
         // # Q1. Given arr[N][M], print row-wise sum.
-        rowSum(arr, sc);
+        rowSum(arr); endOfSegment();
+
+        // # Q2. Given arr[N][M], print column-wise sum.
+        columnWiseSum(arr); endOfSegment();
+
+        // # Q3. Given arr[N][N]. Print Digaonals
+        printDiagonals(arr); endOfSegment();
     }
 }
